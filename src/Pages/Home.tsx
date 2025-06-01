5478820253165deb41627c2287a2fc62b933e89a
agoss-snm <agostinaromontamatt@gmail.com>
2025-06-01 15:24:18 +0200

    firstComponent

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,25 @@
+
+function Home() {
+  return (
+    <>
+      <div className="bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+          <div className="flex lg:flex-1">
+            <a href="." className="-m-1.5 p-1.5">
+              <img
+                alt=""
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+          </div>
+
+          </nav>
+          </header>
+          </div>
+    </>
+  );
+}
+
+export default Home;
